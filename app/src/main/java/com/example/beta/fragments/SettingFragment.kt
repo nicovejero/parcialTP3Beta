bde98d86be3b087ed6d719e4fdedda5d1d9084ff
emipignata <emipignata@gmail.com>
2023-11-03 13:06:13 -0300

    pantalla de inicio maquetado inicial

--- app/src/main/java/com/example/beta/fragments/SettingFragment.kt
+++ app/src/main/java/com/example/beta/fragments/SettingFragment.kt
@@ -1,6 +1,6 @@
-package com.example.beta.fragments
-
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
 import com.example.beta.R
 
@@ -8,5 +8,21 @@ class SettingFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Ocultar la Action Bar cuando se muestra el fragmento
+        val activity = requireActivity() as AppCompatActivity
+        activity.supportActionBar?.hide()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        // Mostrar la Action Bar cuando se sale del fragmento
+        val activity = requireActivity() as AppCompatActivity
+        activity.supportActionBar?.show()
     }
 }
