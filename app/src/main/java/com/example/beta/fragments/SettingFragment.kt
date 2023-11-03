3a1e407a8131ff4c39f3e65cf117c41d9fceeba8
MichelleTicchetti <mticchetti@gmail.com>
2023-11-03 16:51:40 -0300

    dark mode

--- app/src/main/java/com/example/beta/fragments/SettingFragment.kt
+++ app/src/main/java/com/example/beta/fragments/SettingFragment.kt
@@ -1,8 +1,11 @@
 package com.example.beta.fragments
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import com.example.beta.R
 
 class SettingFragment : PreferenceFragmentCompat() {
@@ -25,5 +28,20 @@ class SettingFragment : PreferenceFragmentCompat() {
         // Mostrar la Action Bar cuando se sale del fragmento
         val activity = requireActivity() as AppCompatActivity
         activity.supportActionBar?.show()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toggleNightMode = findPreference<SwitchPreferenceCompat>("dark_mode")
+        toggleNightMode?.setOnPreferenceChangeListener { _, newValue ->
+            val nightMode = if (newValue as Boolean) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        AppCompatDelegate.setDefaultNightMode(nightMode)
+        true
+    }
     }
 }
