584dffbe5798f6903468c40828da365954e3f6a4
emipignata <emipignata@gmail.com>
2023-11-05 12:54:51 -0300

    fix gral

--- app/src/main/java/com/example/beta/ui/view/AdopcionFragment.kt
+++ app/src/main/java/com/example/beta/ui/view/AdopcionFragment.kt
@@ -21,7 +21,7 @@ class AdopcionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_adopcion, container, false)
+        return inflater.inflate(R.layout.fragment_adopcion_detail, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
