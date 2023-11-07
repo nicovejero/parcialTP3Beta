c19596acd8b73abc63b71a41778b092117a74159
MichelleTicchetti <mticchetti@gmail.com>
2023-11-07 15:52:46 -0300

    fix darkmode persistance

--- app/src/main/java/com/example/beta/ParcialTP3.kt
+++ app/src/main/java/com/example/beta/ParcialTP3.kt
@@ -1,6 +1,8 @@
 package com.example.beta
 
 import android.app.Application
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.beta.core.Config
 import dagger.hilt.android.HiltAndroidApp
 
@@ -11,5 +13,15 @@ class ParcialTP3:Application(){
 
         Config.apiKey = resources.getString(R.string.api_key)
         Config.baseUrl = resources.getString(R.string.dogs_api_base_url)
+
+        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val isNightModeEnabled = sharedPreferences.getBoolean("night_mode", false)
+        val nightMode = if (isNightModeEnabled) {
+            AppCompatDelegate.MODE_NIGHT_YES
+        } else {
+            AppCompatDelegate.MODE_NIGHT_NO
+        }
+
+        AppCompatDelegate.setDefaultNightMode(nightMode)
     }
 }
